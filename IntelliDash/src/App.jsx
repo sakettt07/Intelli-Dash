d47ec15fd6df59ae8520f87a7f0e96b87b932cc6
saket <saket@saketgupta.com>
2024-02-20 23:08:38 +0530

    DashBoard Project Setup

---
+++ IntelliDash/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-500 ">
+        <h1>heloo this is me the king on frontend</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
